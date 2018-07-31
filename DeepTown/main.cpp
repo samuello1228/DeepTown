7dd2292a13b4376c884252a058d0d7bfc8df9af3
Samuel Lo <locheukyee2001@gmail.com>
2018-08-01 02:54:10 +0800

    Initial Commit

---
+++ DeepTown/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DeepTown
+//
+//  Created by Samuel Lo on 1/8/2018.
+//  Copyright © 2018年 Samuel Lo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
